2c59151a693b4cf842a75dd3d658de066b0ef43d
Eric Yu <eryu@nvidia.com>
2019-05-12 23:52:51 -0700

    init

---
+++ nan/main.cxx
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+
+
+    {
+        float a = -numeric_limits<float>::infinity();
+        float b = nan("");
+        cout << max(a, b) << endl;
+        cout << "a: " << a << endl;
+        cout << "a == a: " << (a == a) << endl;
+        cout << "b: " << b << endl;
+        cout << "b == b: " << (b == b) << endl;
+        cout << "b > 0: " << (b > 0) << endl;
+        cout << "b < 0: " << (b < 0) << endl;
+        cout << "b == 0: " << (b == 0) << endl;
+    }
+
+    return 0;
+}
---
+++ smart_ptr/main.cxx
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class A {
+public:
+    virtual ~A()
+    {
+        cout << "is a" << endl;
+    }
+};
+class B : public A {
+public:
+    ~B()
+    {
+        cout << "is b" << endl;
+    }
+};
+
+int main()
+{
+    {
+        cout << "test 1" << endl;
+        unique_ptr<A> a;
+        a = unique_ptr<A>(new A);
+    }
+    {
+        cout << "test 2" << endl;
+        unique_ptr<A> a;
+        a = unique_ptr<A>(new B);
+    }
+    {
+        cout << "test 3" << endl;
+        unique_ptr<A> a;
+        a = unique_ptr<B>(new B);
+    }
+
+    return 0;
+}
