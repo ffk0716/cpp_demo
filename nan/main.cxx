59c445374abb3e3dbf2f117a2bf6b4285cbbb350
Eric Yu <eryu@nvidia.com>
2019-07-17 19:55:06 -0700

    modified:   nan/main.cxx

--- nan/main.cxx
+++ nan/main.cxx
@@ -2,20 +2,24 @@
 #include <iostream>
 #include <limits>
 
-using namespace std;
 
+#define TEST_VALUE(exp) std::cout << "(" #exp  ") == " << (exp) << std::endl;
 int main()
 {
-    float a = -numeric_limits<float>::infinity();
-    float b = nan("");
-    cout << max(a, b) << endl;
-    cout << "a: " << a << endl;
-    cout << "a == a: " << (a == a) << endl;
-    cout << "b: " << b << endl;
-    cout << "b == b: " << (b == b) << endl;
-    cout << "b > 0: " << (b > 0) << endl;
-    cout << "b < 0: " << (b < 0) << endl;
-    cout << "b == 0: " << (b == 0) << endl;
+    float inf = std::numeric_limits<float>::infinity();
+    float nan = std::nan("");
+    TEST_VALUE(std::max(inf, nan));
+    TEST_VALUE(inf);
+    TEST_VALUE(inf == inf);
+    TEST_VALUE(nan);
+    TEST_VALUE(nan == nan);
+    TEST_VALUE(nan > 0);
+    TEST_VALUE(nan < 0);
+    TEST_VALUE(nan == 0);
+    TEST_VALUE(nan < inf);
+    TEST_VALUE(inf < nan);
+    TEST_VALUE(nan < -inf ? -inf : nan);
+    TEST_VALUE(std::max(-3.0f, nan));
 
     return 0;
 }
